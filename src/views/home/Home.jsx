45dcf72b7314be899451ebaa539fef1feacc8421
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2024-03-06 16:41:20 +0700

    Adding testimoni section

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,12 @@
-import Hero from "../../parts/home/Hero"
+import Hero from "../../parts/home/Hero";
 import Overview from "../../parts/home/Overview";
+import Testimoni from "../../parts/home/Testimoni";
 export default function Home() {
   return (
     <div>
-      <Hero/>
-      <Overview/>
+      <Hero />
+      <Overview />
+      <Testimoni />
     </div>
   );
 }
